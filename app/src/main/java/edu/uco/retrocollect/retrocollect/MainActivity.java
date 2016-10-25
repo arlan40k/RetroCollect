c38803bd2bb12f4443931ba263907cbc63cea02c
Garrett A. Clement <Arlan40k@gmail.com>
2016-10-25 15:48:43 -0500

    Init commit

---
+++ app/src/main/java/edu/uco/retrocollect/retrocollect/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.uco.retrocollect.retrocollect;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
