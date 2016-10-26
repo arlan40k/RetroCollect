825e11fa0517d32e46b57cfc438bfc63fdc418ef
Garrett A. Clement <Arlan40k@gmail.com>
2016-10-26 15:53:38 -0500

    Added a Local Merchant Activity Carefully this repo thing may end up being tricky

--- app/src/main/java/edu/uco/retrocollect/retrocollect/MainActivity.java
+++ app/src/main/java/edu/uco/retrocollect/retrocollect/MainActivity.java
@@ -1,7 +1,10 @@
 package edu.uco.retrocollect.retrocollect;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ((Button)findViewById(R.id.launchLocalMerchant)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LocalMerchantActivity.class));
+            }
+        });
     }
 }
